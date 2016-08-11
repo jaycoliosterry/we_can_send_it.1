8c3a7a37b7720973d9358947783b800568db9131
Prelang Builder <builder@prelang.com>
2016-08-12 00:34:26 +0200

    Executed 'bundle install' to fetch and install required Ruby Gems

---
+++ lib/easypost/pickup.rb
@@ -0,0 +1,25 @@
+module EasyPost
+  class Pickup < Resource
+
+    def buy(params={})
+      if params.instance_of?(EasyPost::PickupRate)
+        temp = params.clone
+        params = {}
+        params[:carrier] = temp.carrier
+        params[:service] = temp.service
+      end
+
+      response, api_key = EasyPost.request(:post, url + '/buy', @api_key, params)
+      self.refresh_from(response, @api_key, true)
+
+      return self
+    end
+
+    def cancel(params={})
+      response, api_key = EasyPost.request(:post, url + '/cancel', @api_key, params)
+      self.refresh_from(response, @api_key, true)
+
+      return self
+    end
+  end
+end
---
+++ lib/easypost/version.rb
@@ -0,0 +1,3 @@
+module EasyPost
+  VERSION = File.open(File.expand_path("../../../VERSION", __FILE__)).read().strip
+end
