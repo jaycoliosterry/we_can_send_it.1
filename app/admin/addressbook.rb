967613beeab5e7fc867285ec8ffde03f5af03996
Prelang Builder <builder@prelang.com>
2016-08-11 12:26:47 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Addressbook' to allow for create, update

--- app/admin/addressbook.rb
+++ app/admin/addressbook.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Addressbook do
+  permit_params :customer_id, :visible_to_entire_company, :created_by_user_id, :address_book_name, :address_book_description, :archive_this_address_book
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
