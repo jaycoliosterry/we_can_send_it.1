d73e36390720a0e46c50dddead9c73919004d711
Prelang Builder <builder@prelang.com>
2016-08-11 12:26:41 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Company' to allow for create, update

--- app/admin/company.rb
+++ app/admin/company.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Company do
+  permit_params :wcsi_admin, :company_name, :user_admin_id, :department, :shipping_address_line_1, :shipping_address_line_2, :shipping_postcode_or_zip, :shipping_city, :shipping_state_or_region, :shipping_country_id, :billing_address_line_1, :billing_address_line_2, :billing_city, :billing_state_or_region, :billing_country_id, :express_courier_rate_id, :economy_courier_rate_id, :postal_courier_rate_id, :billing_email_address, :main_telephone_number
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
