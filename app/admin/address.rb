8aee03fb457bdc24be651145fecd432bfa512eab
Prelang Builder <builder@prelang.com>
2016-08-11 12:27:14 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Address' to allow for create, update

--- app/admin/address.rb
+++ app/admin/address.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Address do
+  permit_params :receiver_company_id, :receiver_company_name, :receiver_company_contact_name, :address_book_id, :address_line_1, :address_line_2, :zip_or_postcode, :city, :state_or_region, :country_id, :brazilian_federal_tax_id, :brazilian_state_tax_id, :chinese_cr_code
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
