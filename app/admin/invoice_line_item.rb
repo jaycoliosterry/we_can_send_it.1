b6d146a3b8a5794fc9011cc4f31c602a93fcbeca
Prelang Builder <builder@prelang.com>
2016-08-11 12:27:28 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'InvoiceLineItem' to allow for create, update

--- app/admin/invoice_line_item.rb
+++ app/admin/invoice_line_item.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register InvoiceLineItem do
+  permit_params :description, :price, :tax_percentage
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
