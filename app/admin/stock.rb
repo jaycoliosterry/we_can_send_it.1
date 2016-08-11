ac20312f43d78f78d62837f84dcfb9fa8e2686b7
Prelang Builder <builder@prelang.com>
2016-08-11 12:26:32 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Stock' to allow for create, update

--- app/admin/stock.rb
+++ app/admin/stock.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Stock do
+  permit_params :mark_stock_as_to_be_destroyed, :title, :description, :client_reference_code, :height_in_cm, :width_in_cm, :length_in_cm, :paper_size, :number_of_pages, :weight_in_kg, :stock_image, :quantity_currently_in_stock, :total_ever_in_stock, :total_ever_sent, :total_damaged_or_destroyed
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
