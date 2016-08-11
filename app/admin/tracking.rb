5d18b708636b365d52b2f8f9fb8bc1c3129644f9
Prelang Builder <builder@prelang.com>
2016-08-11 12:27:00 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Tracking' to allow for create, update

--- app/admin/tracking.rb
+++ app/admin/tracking.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Tracking do
+  permit_params :signed_for_by, :tracking_code, :tracking_status, :tracking_status_description, :required_to_arrive_before, :will_arrive_in_time
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
