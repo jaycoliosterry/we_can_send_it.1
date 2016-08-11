9c391025fe7c02850c4c280815a42862660a19ca
Prelang Builder <builder@prelang.com>
2016-08-11 12:26:26 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Parcel' to allow for create, update

--- app/admin/parcel.rb
+++ app/admin/parcel.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Parcel do
+  permit_params :customer_shipment_cost_pre_tax, :taxable, :tax_rate, :tax, :customer_shipment_cost_with_tax, :parcel_items_and_quantity, :weight_of_parcel, :customs_short_declaration, :customs_long_description, :parcel_height, :parcel_length, :parcel_width, :parcel_number_in_order, :internal_cost_of_parcel, :shipping_carrier
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
