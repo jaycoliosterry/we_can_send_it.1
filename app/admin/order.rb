312a5da6ebeceb7eee86bba4e717bcde42a21a26
Prelang Builder <builder@prelang.com>
2016-08-11 12:27:24 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Order' to allow for create, update

--- app/admin/order.rb
+++ app/admin/order.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Order do
+  permit_params :delivery_status, :customer_comments, :customer_uploaded_file, :send_confirmation_emails_to_recipients, :order_number, :total_order_weight, :wcsi_total_order_cost, :customer_total_order_cost_pre_tax, :any_taxable_shipments, :total_tax_on_order, :customer_total_order_cost_with_tax
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
