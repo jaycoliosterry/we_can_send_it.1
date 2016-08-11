2d8333857a9293b3bc159dc1916598c8b244fb07
Prelang Builder <builder@prelang.com>
2016-08-11 18:18:08 +0200

    Executed 'bundle install' to fetch and install required Ruby Gems

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -1,7 +1,10 @@
 class PricesController < ApplicationController
   def index
+    @prices = Price.all
   end
 
   def import
+    Price.import(params[:file])
+    redirect_to root_url, notice: "Prices imported."
   end
 end
