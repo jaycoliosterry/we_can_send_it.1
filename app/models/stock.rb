0cba8fdc0ebeda39385903a7b669ae7a51c63e82
Prelang Builder <builder@prelang.com>
2016-08-11 22:58:26 +0200

    Executed 'bundle install' to fetch and install required Ruby Gems

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,4 +5,9 @@ class Stock < ActiveRecord::Base
   has_attached_file :image
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-end
+  before_save :set_total_ever_in_stock
+
+  def set_total_ever_in_stock()
+    self.total_ever_in_stock = self.total_ever_in_stock + self.quantity_currently_in_stock
+	end
+  end
