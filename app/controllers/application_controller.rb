595c5bbb1726470913cf80950cb71552a7c3d6a7
Prelang Builder <builder@prelang.com>
2016-08-13 00:09:01 +0200

    Executed 'bundle install' to fetch and install required Ruby Gems

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_in)        { |u| u.permit(:login, :username, :email, :password, :remember_me) }
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
   end
-
+  
+  include Pundit
+    protect_from_forgery
+  
 
   private
   
