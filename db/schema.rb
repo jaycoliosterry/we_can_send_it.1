face4ba3e999d572a95b236ffad96335385bd27c
Prelang Builder <builder@prelang.com>
2016-08-15 11:43:41 +0200

    Executed 'rake db:migrate' (development and test) to migrate all generated migrations which created schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160811230630) do
+ActiveRecord::Schema.define(version: 20160815094323) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -205,6 +205,50 @@ ActiveRecord::Schema.define(version: 20160811230630) do
 
   add_index "invoices_orders", ["invoice_id"], name: "index_invoices_orders_on_invoice_id"
   add_index "invoices_orders", ["order_id"], name: "index_invoices_orders_on_order_id"
+
+  create_table "invoicing_ledger_items", force: true do |t|
+    t.integer  "sender_id"
+    t.integer  "recipient_id"
+    t.string   "type"
+    t.datetime "issue_date"
+    t.string   "currency",     limit: 3,                           null: false
+    t.decimal  "total_amount",            precision: 20, scale: 4
+    t.decimal  "tax_amount",              precision: 20, scale: 4
+    t.string   "status",       limit: 20
+    t.string   "identifier",   limit: 50
+    t.string   "description"
+    t.datetime "period_start"
+    t.datetime "period_end"
+    t.string   "uuid",         limit: 40
+    t.datetime "due_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "invoicing_line_items", force: true do |t|
+    t.integer  "ledger_item_id"
+    t.string   "type"
+    t.decimal  "net_amount",                precision: 20, scale: 4
+    t.decimal  "tax_amount",                precision: 20, scale: 4
+    t.string   "description"
+    t.string   "uuid",           limit: 40
+    t.datetime "tax_point"
+    t.decimal  "quantity",                  precision: 20, scale: 4
+    t.integer  "creator_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "invoicing_tax_rates", force: true do |t|
+    t.string   "description"
+    t.decimal  "rate",           precision: 20, scale: 4
+    t.datetime "valid_from",                              null: false
+    t.datetime "valid_until"
+    t.integer  "replaced_by_id"
+    t.boolean  "is_default"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "orders", force: true do |t|
     t.integer  "invoice_line_item_id"
